954cdaca70f0313414fda27dfd791cc7ff74fada
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-06-23 03:03:27 +0300

    Add chat model for storing chats

---
+++ app/src/main/java/com/moringaschool/campuschat/Models/Chat.java
@@ -0,0 +1,54 @@
+package com.moringaschool.campuschat.Models;
+
+public class Chat {
+
+    String mobile, name, message, date, time;
+
+    public Chat(String mobile, String name, String message, String date, String time) {
+        this.mobile = mobile;
+        this.name = name;
+        this.message = message;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
